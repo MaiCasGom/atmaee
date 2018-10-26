afc8181ef1a1b4c58a1efb658e8677a89c73a087
Mai <maicasgom@gmail.com>
2018-10-26 20:11:35 +0200

    Graficas funcionando

--- DatosVivienda.R
+++ DatosVivienda.R
@@ -1,10 +1,10 @@
+library(lubridate)
 setwd("/home/mai/Escritorio/TFG")
 DatosVivienda= read.csv("household_power_consumption.txt", comment.char = "@", sep = ";", na.strings=c("?","NA"))
 #colnames(DatosVivienda) = c("Fecha","Hora", "PotenciaActivaGlobal (kW)",
  #                    "PotenciaReactivaGlobal (kW)", "Voltaje (V)",
 #                     "IntensidadGlobal (A)","ConsumoCocina (w/h)","ConsumoLavanderia (w/h)","ConsumoAireyCalentador (w/h)")
 head(DatosVivienda)
-class(DatosViviendaAgrupados$date)
 #dim(DatosVivienda)
 #str(DatosVivienda)
 #summary(DatosVivienda)
@@ -12,3 +12,4 @@ Suma_Total <- rowSums(DatosVivienda[, c(7, 8, 9)], na.rm = TRUE)
 DatosViviendaAgrupados <- data.frame(date=DatosVivienda$Date, consumo_total=Suma_Total)
 Suma_Total_by_date <- aggregate(DatosViviendaAgrupados$consumo_total, list(date=DatosViviendaAgrupados$date), FUN=sum)
 Suma_Total_by_date$date=as.Date(Suma_Total_by_date$date, format = "%d/%m/%Y")
+with( Suma_Total_by_date[ year(Suma_Total_by_date$date) =="2006",  ] , plot(date , x  , type = "l") )
