f678878e022d18855a2c848b32c00f26645c5d7a
María Isabel Castillo Gómez <maicasgom@gmail.com>
2020-07-13 18:16:35 +0200

    Update

--- consumo_por_meses.R
+++ consumo_por_meses.R
@@ -6,7 +6,7 @@ DatosVivienda <- na.omit(DatosVivienda)
 
 colnames(DatosVivienda) = c("Fecha","Hora", "PotenciaActivaGlobal (kW)",
                             "PotenciaReactivaGlobal (kW)", "Voltaje (V)",
-                            "IntensidadGlobal (A)","ConsumoCocina (w/h)","ConsumoLavanderia (w/h)","ConsumoAireyCalentador (w/h)")
+                            "IntensidadGlobal (A)","ConsumoCocina (Wh)","ConsumoLavanderia (Wh)","ConsumoAireyCalentador (Wh)")
 
 # Sumamos el consumo de cocina, lavanderia y aires acondicionados y si falta algun dato no lo tenemos en cuenta.
 SumaTotal <- rowSums(DatosVivienda[, c(7, 8, 9)])
@@ -17,14 +17,14 @@ SumaTotalPorFecha <- aggregate(DatosViviendaAgrupados$consumo_total, list(date=D
 SumaTotalPorFecha$date=as.Date(SumaTotalPorFecha$date, format = "%d/%m/%Y")
 
 anios <- sort(unique(year(SumaTotalPorFecha$date)))
-pdf("Consumo mensual.pdf",width=6,height=4,paper='special')
+pdf("graficas/consumo_mensual.pdf",width=6,height=4,paper='special')
 for(y in anios){
   datosAnio <- subset(SumaTotalPorFecha, year(SumaTotalPorFecha$date) == y) 
   meses <- sort(unique(month(datosAnio$date)))
   
   for(m in meses){
     datosMes <- subset(datosAnio, month(datosAnio$date) == m)
-    print(quickplot(datosMes$date ,datosMes$x , geom = c("line"), xlab = "", ylab = "Consumo (w/h)", ylim = c(-10000,35000), main = paste("Año", y, sep = " ")) +  
+    print(quickplot(datosMes$date ,datosMes$x , geom = c("line"), xlab = "", ylab = "Consumo (Wh)", ylim = c(-10000,35000), main = paste("Año", y, sep = " ")) +  
             geom_point()+ geom_smooth(level = 0.95))
   }
 }
--- consumo_trimestral.R
+++ consumo_trimestral.R
@@ -5,7 +5,7 @@ library(zoo)
 DatosVivienda= read.csv("household_power_consumption.txt", comment.char = "@", sep = ";")
 colnames(DatosVivienda) = c("Fecha","Hora", "PotenciaActivaGlobal (kW)",
                             "PotenciaReactivaGlobal (kW)", "Voltaje (V)",
-                            "IntensidadGlobal (A)","ConsumoCocina (w/h)","ConsumoLavanderia (w/h)","ConsumoAireyCalentador (w/h)")
+                            "IntensidadGlobal (A)","ConsumoCocina (Wh)","ConsumoLavanderia (Wh)","ConsumoAireyCalentador (Wh)")
 
 DatosVivienda <- na.omit(DatosVivienda)
 # Sumamos el consumo de cocina, lavanderia y aires acondicionados y si falta algún dato no lo tenemos en cuenta.
@@ -19,7 +19,7 @@ SumaTotalPorFecha$date=as.Date(SumaTotalPorFecha$date, format = "%d/%m/%Y")
 anios <- sort(unique(year(SumaTotalPorFecha$date)))
 meses <- sort(unique(month(SumaTotalPorFecha$date)))
 
-pdf("Consumo trimestres.pdf",width=6,height=4,paper='special')
+pdf("graficas/consumo_trimestre.pdf",width=6,height=4,paper='special')
 for(m in c(1,4,7,10)){
   datosTrimestre <- subset(SumaTotalPorFecha, (month(SumaTotalPorFecha$date) == m | month(SumaTotalPorFecha$date) == m+1 |  month(SumaTotalPorFecha$date) == m+2 ))
   matrizDatosTrimestre <- matrix(nrow=93, ncol=length(anios))
@@ -35,7 +35,7 @@ for(m in c(1,4,7,10)){
   nn <- ncol(matrizDatosTrimestre)
   layout(matrix(c(1,2),nrow=1), width=c(3,1)) 
   par(mar=c(4,3,3,0)) #No margin on the right side
-  print(matplot(na.approx(matrizDatosTrimestre), type="l", lty = "solid", col = 1:6, xlab=paste(month.abb[m], month.abb[m+2], sep=" - "), ylab="Consumo (w/h)", ylim = c(0,35000)))
+  print(matplot(na.approx(matrizDatosTrimestre), type="l", lty = "solid", col = 1:6, xlab=paste(month.abb[m], month.abb[m+2], sep=" - "), ylab="Consumo (Wh)", ylim = c(0,35000)))
   print(plot(c(0,1),type="n", axes=F, xlab="", ylab=""))
   print(legend("left", colnames(matrizDatosTrimestre),col=seq_len(nn),cex=0.8,fill=seq_len(nn)))
 }
