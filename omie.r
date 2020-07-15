8da30e51ccd1ab4581f5b719d0f694ddabfdeefa
María Isabel Castillo Gómez <maicasgom@gmail.com>
2020-07-15 18:26:43 +0200

    Añadido script para curva monotona

--- omie.r
+++ omie.r
@@ -6,7 +6,7 @@ files <- list.files(path="OMIE", pattern="*", full.names=TRUE, recursive=FALSE)
 
 DatosOmie <- lapply(files, function(x) {
   t <- read.csv(x, header= FALSE, sep = ";", comment.char = "*", skip = 1)
-  t <- c(mean(t$V1), mean(t$V2), mean(t$V3), mean(t$V5))
+ # t <- c(mean(t$V1), mean(t$V2), mean(t$V3), mean(t$V5))
 })
 
 DatosOmie <- do.call(rbind, DatosOmie)
@@ -19,7 +19,7 @@ colnames(DatosOmie) = c("Anio","Mes", "Dia", "PrecioES")
 DatosVivienda= read.csv("household_power_consumption.txt", comment.char = "@", sep = ";", na.strings=c("?","NA"))
 colnames(DatosVivienda) = c("Fecha","Hora", "PotenciaActivaGlobal (kW)",
                             "PotenciaReactivaGlobal (kW)", "Voltaje (V)",
-                            "IntensidadGlobal (A)","ConsumoCocina (w/h)","ConsumoLavanderia (w/h)","ConsumoAireyCalentador (w/h)")
+                            "IntensidadGlobal (A)","ConsumoCocina (Wh)","ConsumoLavanderia (Wh)","ConsumoAireyCalentador (Wh)")
 
 DatosVivienda <- na.omit(DatosVivienda)
 # Sumamos el consumo de cocina, lavanderia y aires acondicionados y si falta algún dato no lo tenemos en cuenta.
