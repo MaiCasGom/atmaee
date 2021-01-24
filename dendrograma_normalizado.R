915063dac86127e2fdc59a2f928ace8abe28a0ec
María Isabel Castillo Gómez <maicasgom@gmail.com>
2021-01-24 18:10:53 +0100

    Update

--- dendrograma_normalizado.R
+++ dendrograma_normalizado.R
@@ -1,7 +1,18 @@
+###### Funcion para normalizacion ####
+
+min_max_norm <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+######################################
+
+library(cluster)
 DatosViviendaAgrupados <- subset( DatosViviendaAgrupados, select = -Fecha )
 DatosViviendaAgrupados <- subset( DatosViviendaAgrupados, select = -Hora )
 
-DatosViviendaRangos <-apply(DatosViviendaAgrupados,2,range)
+DatosViviendaAgrupadosNorm <- as.data.frame(lapply(DatosViviendaAgrupados[1:9], min_max_norm))
+
+DatosViviendaRangos <-apply(DatosViviendaAgrupadosNorm,2,range)
 
 DatosViviendaRangos <- mapply(DatosViviendaRangos, FUN=as.numeric)
 DatosViviendaRangos <- matrix(data=DatosViviendaRangos, ncol=9, nrow=2)
@@ -9,108 +20,120 @@ colnames(DatosViviendaRangos) = c("ConsumoCocina","ConsumoLavanderia", "ConsumoA
                             "ConsumoTotal", "Precio", "GastoCocina","GastoLavanderia","GastoAireyCalefaccion","GastoTotal")
 ## General ##
 # La distancia ponderada es igual a la distancia numérica al no existir variables binarias
-dist_ponderada = dist(DatosViviendaAgrupados)
+dist_ponderada = dist(DatosViviendaAgrupadosNorm)
 hGeneral = hclust(dist_ponderada, method = "ward.D2")
 
 ## Consumo Cocina ##
-DatosConsumoCocina <- DatosViviendaAgrupados$ConsumoCocina
+DatosConsumoCocina <- DatosViviendaAgrupadosNorm$ConsumoCocina
 dist_ponderada = dist(DatosConsumoCocina)
 hCCocina = hclust(dist_ponderada, method = "ward.D2")
 
 ## Consumo Lavanderia ##
-DatosConsumoLavanderia <- DatosViviendaAgrupados$ConsumoLavanderia
+DatosConsumoLavanderia <- DatosViviendaAgrupadosNorm$ConsumoLavanderia
 dist_ponderada = dist(DatosConsumoLavanderia)
 hCLavanderia = hclust(dist_ponderada, method = "ward.D2")
 
 ## Consumo Aire y Calefaccion ##
-DatosConsumoAireyCalefaccion <- DatosViviendaAgrupados$ConsumoAireyCalefaccion
+DatosConsumoAireyCalefaccion <- DatosViviendaAgrupadosNorm$ConsumoAireyCalefaccion
 dist_ponderada = dist(DatosConsumoAireyCalefaccion)
 hCAireyCalefaccion = hclust(dist_ponderada, method = "ward.D2")
 
 ## Consumo Total ##
-DatosConsumoTotal<- DatosViviendaAgrupados$ConsumoTotal
+DatosConsumoTotal<- DatosViviendaAgrupadosNorm$ConsumoTotal
 dist_ponderada = dist(DatosConsumoTotal)
 hCTotal = hclust(dist_ponderada, method = "ward.D2")
 
 ## Consumo Cocina + Lavanderia + Aire y Calefacción ##
-DatosConsumoCocinaLavanderiaAireCalefaccion <- subset( DatosViviendaAgrupados, select = c("ConsumoCocina", "ConsumoLavanderia", "ConsumoAireyCalefaccion") )
+DatosConsumoCocinaLavanderiaAireCalefaccion <- subset( DatosViviendaAgrupadosNorm, select = c("ConsumoCocina", "ConsumoLavanderia", "ConsumoAireyCalefaccion") )
 dist_ponderada = dist(DatosConsumoCocinaLavanderiaAireCalefaccion)
 hCCocinaLavanderiaAireCalefaccion = hclust(dist_ponderada, method = "ward.D2")
 
 ## Precio ##
-DatosPrecio<- DatosViviendaAgrupados$Precio
+DatosPrecio<- DatosViviendaAgrupadosNorm$Precio
 dist_ponderada = dist(DatosPrecio)
 hPrecio = hclust(dist_ponderada, method = "ward.D2")
 
 ## Gasto Cocina ##
-DatosGastoCocina<- DatosViviendaAgrupados$GastoCocina
+DatosGastoCocina<- DatosViviendaAgrupadosNorm$GastoCocina
 dist_ponderada = dist(DatosGastoCocina)
 hGCocina = hclust(dist_ponderada, method = "ward.D2")
 
 ## Gasto Lavanderia ##
-DatosGastoLavanderia<- DatosViviendaAgrupados$GastoLavanderia
+DatosGastoLavanderia<- DatosViviendaAgrupadosNorm$GastoLavanderia
 dist_ponderada = dist(DatosGastoLavanderia)
 hGLavanderia = hclust(dist_ponderada, method = "ward.D2")
 
 ## Gasto AireyCalefaccion ##
-DatosGastoAireyCalefaccion<- DatosViviendaAgrupados$GastoAireyCalefaccion
+DatosGastoAireyCalefaccion<- DatosViviendaAgrupadosNorm$GastoAireyCalefaccion
 dist_ponderada = dist(DatosGastoAireyCalefaccion)
 hGAireyCalefaccion = hclust(dist_ponderada, method = "ward.D2")
 
 ## Gasto Total ##
-DatosGastoTotal<- DatosViviendaAgrupados$GastoTotal
+DatosGastoTotal<- DatosViviendaAgrupadosNorm$GastoTotal
 dist_ponderada = dist(DatosGastoTotal)
 hGTotal = hclust(dist_ponderada, method = "ward.D2")
 
 ## Gasto Cocina + Lavanderia + Aire y Calefacción ##
-DatosGastooCocinaLavanderiaAireCalefaccion <- subset( DatosViviendaAgrupados, select = c("GastoCocina", "GastoLavanderia", "GastoAireyCalefaccion") )
+DatosGastooCocinaLavanderiaAireCalefaccion <- subset( DatosViviendaAgrupadosNorm, select = c("GastoCocina", "GastoLavanderia", "GastoAireyCalefaccion") )
 dist_ponderada = dist(DatosGastooCocinaLavanderiaAireCalefaccion)
 hGCocinaLavanderiaAireCalefaccion = hclust(dist_ponderada, method = "ward.D2")
 
-jpeg("graficas/dendrogramas/general.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/general.jpg",width=1080,height=720)
 plot(hGeneral, main="Dendrograma general")
+rect.hclust(hGeneral, k = 3) 
 dev.off()
 
-jpeg("graficas/dendrogramas/consumo_cocina.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/consumo_cocina.jpg",width=1080,height=720)
 plot(hCCocina, main="Dendrograma Consumo Cocina")
+rect.hclust(hCCocina, k = 2) 
 dev.off()
 
-jpeg("graficas/dendrogramas/consumo_lavanderia.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/consumo_lavanderia.jpg",width=1080,height=720)
 plot(hCLavanderia, main="Dendrograma Consumo Lavanderia")
+rect.hclust(hCLavanderia, k = 2) 
 dev.off()
 
-jpeg("graficas/dendrogramas/consumo_aire_calefaccion.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/consumo_aire_calefaccion.jpg",width=1080,height=720)
 plot(hCAireyCalefaccion, main="Dendrograma Consumo Aire y Calefaccion")
+rect.hclust(hCAireyCalefaccion, k = 3) 
 dev.off()
 
-jpeg("graficas/dendrogramas/consumo_total.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/consumo_total.jpg",width=1080,height=720)
 plot(hCTotal, main="Dendrograma Consumos totales")
+rect.hclust(hCTotal, k = 4) 
 dev.off()
 
-jpeg("graficas/dendrogramas/general_consumos.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/general_consumos.jpg",width=1080,height=720)
 plot(hCCocinaLavanderiaAireCalefaccion, main="Dendrograma general de consumos")
+rect.hclust(hCCocinaLavanderiaAireCalefaccion, k = 3) 
 dev.off()
 
-jpeg("graficas/dendrogramas/precios.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/precios.jpg",width=1080,height=720)
 plot(hPrecio, main="Dendrograma Precio")
+rect.hclust(hPrecio, k = 4) 
 dev.off()
 
-jpeg("graficas/dendrogramas/gasto_cocina.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/gasto_cocina.jpg",width=1080,height=720)
 plot(hGCocina, main="Dendrograma Gasto Cocina")
+rect.hclust(hGCocina, k = 2) 
 dev.off()
 
-jpeg("graficas/dendrogramas/gasto_lavanderia.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/gasto_lavanderia.jpg",width=1080,height=720)
 plot(hGLavanderia, main="Dendrograma Gasto Lavanderia")
+rect.hclust(hGLavanderia, k = 2) 
 dev.off()
 
-jpeg("graficas/dendrogramas/gasto_aire_calefaccion.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/gasto_aire_calefaccion.jpg",width=1080,height=720)
 plot(hGAireyCalefaccion, main="Dendrograma Gasto Aire y Calefaccion")
+rect.hclust(hGAireyCalefaccion, k = 4) 
 dev.off()
 
-jpeg("graficas/dendrogramas/gasto_total.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/gasto_total.jpg",width=1080,height=720)
 plot(hGTotal, main="Dendrograma Gasto total")
+rect.hclust(hGTotal, k = 2) 
 dev.off()
 
-jpeg("graficas/dendrogramas/general_gastos.jpg",width=1080,height=720)
+jpeg("graficas/dendrogramas_normalizados/general_gastos.jpg",width=1080,height=720)
 plot(hGCocinaLavanderiaAireCalefaccion, main="Dendrograma general de gastos")
+rect.hclust(hGCocinaLavanderiaAireCalefaccion, k = 5) 
 dev.off()
---
+++ kmedioides.R
@@ -0,0 +1,107 @@
+###### Funcion para normalizacion ####
+
+min_max_norm <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+######################################
+
+library(cluster)
+
+DatosViviendaAgrupados <- subset( DatosViviendaAgrupados, select = -Fecha )
+DatosViviendaAgrupados <- subset( DatosViviendaAgrupados, select = -Hora )
+
+DatosViviendaAgrupadosNorm <- as.data.frame(lapply(DatosViviendaAgrupados[1:9], min_max_norm))
+
+DatosViviendaRangos <-apply(DatosViviendaAgrupadosNorm,2,range)
+
+DatosViviendaRangos <- mapply(DatosViviendaRangos, FUN=as.numeric)
+DatosViviendaRangos <- matrix(data=DatosViviendaRangos, ncol=9, nrow=2)
+colnames(DatosViviendaRangos) = c("ConsumoCocina","ConsumoLavanderia", "ConsumoAireyCalefaccion",
+                                  "ConsumoTotal", "Precio", "GastoCocina","GastoLavanderia","GastoAireyCalefaccion","GastoTotal")
+
+k = 7
+
+## Consumo Cocina ##
+DatosConsumoCocina <- DatosViviendaAgrupadosNorm$ConsumoCocina
+dist_ponderada = dist(DatosConsumoCocina)
+#jpeg("graficas/silhouette/consumo_cocina_kmedioides.jpg",width=1080,height=720)
+#plot(1, type="n", xlab="", ylab="", xlim=c(1, k+1), ylim=c(0.92, 0.96), main="Cálculo factor K - Consumo Cocina")
+#for (i in 2:k){ 
+  km <- pam(DatosConsumoCocina, 7)
+  ss <- silhouette(km$cluster, dist_ponderada)
+  coeficiente_silhouette = mean(ss[, 3])
+#  points(i,mean(ss[, 3]))
+ # text(i, mean(ss[, 3]), round(mean(ss[, 3]), 4), cex=0.8, pos = 3)
+#}
+#dev.off()
+
+## Consumo Lavanderia ##
+DatosConsumoLavanderia <- DatosViviendaAgrupadosNorm$ConsumoLavanderia
+dist_ponderada = dist(DatosConsumoLavanderia)
+#jpeg("graficas/silhouette/consumo_lavanderia_kmedioides.jpg",width=1080,height=720)
+#plot(1, type="n", xlab="", ylab="", xlim=c(1, k+1), ylim=c(0.6, 0.96), main="Cálculo factor K - Consumo Lavandería")
+#for (i in 2:k){ 
+  km <- pam(DatosConsumoLavanderia, 7)
+  ss <- silhouette(km$cluster, dist_ponderada)
+  coeficiente_silhouette = mean(ss[, 3])
+#  points(i,mean(ss[, 3]))
+#  text(i, mean(ss[, 3]), round(mean(ss[, 3]), 4), cex=0.8, pos = 3)
+#}
+#dev.off()
+
+## Consumo Aire y Calefaccion ##
+DatosConsumoAireyCalefaccion <- DatosViviendaAgrupadosNorm$ConsumoAireyCalefaccion
+dist_ponderada = dist(DatosConsumoAireyCalefaccion)
+#jpeg("graficas/silhouette/consumo_aireycalefaccion_kmedioides.jpg",width=1080,height=720)
+#plot(1, type="n", xlab="", ylab="", xlim=c(1, k+1), ylim=c(0.60, 0.85), main="Cálculo factor K - Consumo Aire y Calefaccion")
+#for (i in 2:k){ 
+  km <- pam(DatosConsumoAireyCalefaccion, 7)
+  ss <- silhouette(km$cluster, dist_ponderada)
+  coeficiente_silhouette = mean(ss[, 3])
+#  points(i,mean(ss[, 3]))
+#  text(i, mean(ss[, 3]), round(mean(ss[, 3]), 4), cex=0.8, pos = 3)
+#}
+#dev.off()
+
+## Consumo Total ##
+DatosConsumoTotal<- DatosViviendaAgrupadosNorm$ConsumoTotal
+dist_ponderada = dist(DatosConsumoTotal)
+#jpeg("graficas/silhouette/consumo_total_kmedioides.jpg",width=1080,height=720)
+#plot(1, type="n", xlab="", ylab="", xlim=c(1, k+1), ylim=c(0.60, 0.85), main="Cálculo factor K - Consumo Total")
+#for (i in 2:k){ 
+  km <- pam(DatosConsumoTotal, 7)
+  ss <- silhouette(km$cluster, dist_ponderada)
+  coeficiente_silhouette = mean(ss[, 3])
+#  points(i,mean(ss[, 3]))
+#  text(i, mean(ss[, 3]), round(mean(ss[, 3]), 4), cex=0.8, pos = 3)
+#}
+#dev.off()
+
+## Consumo Cocina + Lavanderia + Aire y Calefacción ##
+DatosConsumoCocinaLavanderiaAireCalefaccion <- subset( DatosViviendaAgrupadosNorm, select = c("ConsumoCocina", "ConsumoLavanderia", "ConsumoAireyCalefaccion") )
+dist_ponderada = dist(DatosConsumoCocinaLavanderiaAireCalefaccion)
+#jpeg("graficas/silhouette/consumo_general_kmedioides.jpg",width=1080,height=720)
+#plot(1, type="n", xlab="", ylab="", xlim=c(1, k+1), ylim=c(0.40, 0.80), main="Cálculo factor K - Consumo General")
+#for (i in 2:k){ 
+  km <- pam(DatosConsumoCocinaLavanderiaAireCalefaccion, 7)
+  ss <- silhouette(km$cluster, dist_ponderada)
+  coeficiente_silhouette = mean(ss[, 3])
+#  points(i,mean(ss[, 3]))
+#  text(i, mean(ss[, 3]), round(mean(ss[, 3]), 4), cex=0.8, pos = 3)
+#}
+#dev.off()
+
+## Precio ##
+DatosPrecio<- DatosViviendaAgrupadosNorm$Precio
+dist_ponderada = dist(DatosPrecio)
+#jpeg("graficas/silhouette/precio_kmedioides.jpg",width=1080,height=720)
+#plot(1, type="n", xlab="", ylab="", xlim=c(1, k+1), ylim=c(0, 1), main="Cálculo factor K - Precio")
+#for (i in 2:k){ 
+  km <- pam(DatosPrecio, 7)
+  ss <- silhouette(km$cluster, dist_ponderada)
+  coeficiente_silhouette = mean(ss[, 3])
+#  points(i,mean(ss[, 3]))
+#  text(i, mean(ss[, 3]), round(mean(ss[, 3]), 4), cex=0.8, pos = 3)
+#}
+#dev.off()
